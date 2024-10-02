11a8718ee2617ea1f97a3853524744763a42007b
akshararee <aksharasreesampath@gmail.com>
2024-10-03 02:03:23 +0530

    task 9 added

--- routes/adoptionRoutes.js
+++ routes/adoptionRoutes.js
@@ -18,5 +18,32 @@ router.get("/adopt_g", async (req, res) => {
     res.send(error);
   }
 });
+router.put("/adopt_put/:id", async (req, res) => {
+  try {
+    const adopt3 = await adoption_model.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+    if (!adopt3)
+      return res.status(404).send({ message: "Adoption cannot be recorded" });
+    res.status(200).send(adopt3);
+  } catch (error) {
+    res.send(error);
+  }
+});
+router.delete("/adopt_d/:id", async (req, res) => {
+  try {
+    const adopt4 = await adoption_model.findByIdAndDelete(req.params.id);
+    if (!adopt4)
+      return res.status(404).send({ message: "Adoption cannot be found" });
+    res.status(200).send({ message: "Adoption deleted successfully" });
+  } catch (error) {
+    res.send(error);
+  }
+});
 
 export default router;
